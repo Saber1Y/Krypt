7c5572f9207b7bc015f6c815c3d505bdf9817bcb
ayodeji <ayodejiakintobi1@gmail.com>
2023-12-01 09:13:22 +0100

    UPDATE: COMP INITIALIZATION

---
+++ clients/src/components/Index.js
@@ -0,0 +1,5 @@
+export { default as NavBar } from './NavBar';
+export { default as Service } from './Service';
+export { default as Welcome } from './Welcome';
+export { default as Loader } from './Loader';
+export { default as Footer} from './Footer';
